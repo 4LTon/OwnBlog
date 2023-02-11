8fc01b7e2ebb986e7bf5f3aae217f6765d74fb4b
Vladislav Glotov <glotov.vd@yandex.ru>
2023-02-11 08:12:22 +0300

    init: server module

---
+++ server/src/main/java/edu/ownblog/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package edu.ownblog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
