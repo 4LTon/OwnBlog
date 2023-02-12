a0a726bd2b73de01ffa153757261f5fc7172340d
Vladislav Glotov <glotov.vd@yandex.ru>
2023-02-12 06:59:48 +0300

    fix <repositories.PostRepository>: добавлена аннотация @Repository

--- server/src/main/java/edu/ownblog/server/repositories/PostRepository.java
+++ server/src/main/java/edu/ownblog/server/repositories/PostRepository.java
@@ -2,11 +2,13 @@ package edu.ownblog.server.repositories;
 
 import edu.ownblog.server.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Vladislav Glotov <glotov.vd@yandex.ru>
  * @version 1.0.0
  */
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
